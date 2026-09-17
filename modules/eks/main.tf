9b162f1e245cd2685adc49e492bad787249c8845
igorcorrean <igorconunes@gmail.com>
2026-09-17 16:16:17 -0300

    Altera a quantidade de pods

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -123,9 +123,9 @@ resource "aws_eks_node_group" "this" {
 
   # 5. Escala enxuta: 1 nó para rodar pods essenciais (CoreDNS/kube-proxy/workloads)
   scaling_config {
-    desired_size = 2
+    desired_size = 1
     min_size     = 1
-    max_size     = 3
+    max_size     = 2
   }
 
   update_config {
