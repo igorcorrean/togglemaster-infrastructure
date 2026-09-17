69c4144ab24e97de62dd193c69a22b542cb94c1b
igorcorrean <igorconunes@gmail.com>
2026-09-17 12:55:23 -0300

    Altera o tipo de instancia para receber mais pods

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -112,7 +112,7 @@ resource "aws_eks_node_group" "this" {
   ami_type = "AL2023_x86_64_STANDARD"
 
   # 2. Pool diversificado em instâncias burstable ARM
-  instance_types = ["t3.small"]
+  instance_types = ["t3.medium","t3.small"]
 
   # 3. Economia de até 90% com instâncias Spot
   capacity_type = "SPOT"
