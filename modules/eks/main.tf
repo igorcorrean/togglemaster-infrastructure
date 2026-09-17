e0df357343fbcded5613fd754719162a0555dfcf
igorcorrean <igorconunes@gmail.com>
2026-09-17 13:39:13 -0300

    aumento temporario do scaling config

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -123,9 +123,9 @@ resource "aws_eks_node_group" "this" {
 
   # 5. Escala enxuta: 1 nó para rodar pods essenciais (CoreDNS/kube-proxy/workloads)
   scaling_config {
-    desired_size = 1
+    desired_size = 2
     min_size     = 1
-    max_size     = 2
+    max_size     = 3
   }
 
   update_config {
